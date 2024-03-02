309a2229202f7b9c520c93e26ccada4a99ab3162
dnatividade <43869367+dnatividade@users.noreply.github.com>
2024-03-01 21:30:23 -0300

    Atualizar o nat-hairpinning.sh

--- iptables/nat-hairpinning.sh
+++ iptables/nat-hairpinning.sh
@@ -12,6 +12,7 @@ IP_PRIVATE=192.168.1.1/32
 IP_SERVER=192.168.1.200/32
 LAN=192.168.1.0/24
 
+
 iptables -t nat -A PREROUTING  -i $IF_LAN -s $LAN -d $IP_PUBLIC -p tcp -m tcp --dport 80 -j DNAT --to $IP_SERVER
 iptables -t nat -A POSTROUTING -o $IF_LAN -s $LAN -d $IP_SERVER -p tcp -m tcp --dport 80 -j SNAT --to $IP_PRIVATE
 iptables -A FORWARD -i $IF_LAN -o $IF_LAN -j ACCEPT
